c551dad7895177600795a571a1440bd0b1b0385d
Alessandro Maclaine <almaclaine@gmail.com>
2023-04-29 03:07:53 -0500

    progress

--- packages/stepci/src/generators/generate/generator.ts
+++ packages/stepci/src/generators/generate/generator.ts
@@ -1,19 +1,30 @@
 import {
+  addDependenciesToPackageJson,
   extractLayoutDirectory,
   getPackageManagerCommand,
+  logger,
   Tree,
 } from '@nx/devkit';
 import * as path from 'path';
 import { GenerateGeneratorSchema } from './schema';
-import { getAppsDir } from '@almaclaine/nx';
+import { getLibsDir, hasProjectOrFail } from '@almaclaine/nx';
 
-export default async function (tree: Tree, options: GenerateGeneratorSchema) {
-  console.log(getPackageManagerCommand().run('test', ''));
-  // console.log(getProjects(tree).has('canvas-client'));
-  // console.log(getProjects(tree).has('stepci'));
-  console.log(extractLayoutDirectory(tree.root));
-  throw Error('awd');
-  // const projectRoot = `libs/${options.name}`;
+export default async function (tree: Tree, { name }: GenerateGeneratorSchema) {
+  hasProjectOrFail(tree, name);
+  const libsDir = getLibsDir(tree);
+  logger.info(`libsDir: ${libsDir}`);
+  const projectRoot = `${libsDir}/${name}`;
+  logger.info(`projectRoot: ${projectRoot}`);
+  const installTask = addDependenciesToPackageJson(
+    tree,
+    {},
+    {
+      stepci: '2.6.6',
+    }
+  );
+
+  logger.info('Installing stepci dependencies');
+  await installTask();
   // addProjectConfiguration(tree, options.name, {
   //   root: projectRoot,
   //   projectType: 'library',
