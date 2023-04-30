f515a7c7fe1508b4573b04abfb92e698577a53b7
Alessandro Maclaine <almaclaine@gmail.com>
2023-04-30 01:42:36 -0500

    finish executor

---
+++ packages/stepci/src/executors/run/executor.ts
@@ -0,0 +1,27 @@
+import type { ExecutorContext } from '@nx/devkit';
+import { RunExecutorSchema } from './schema';
+import { logger } from '@nx/devkit';
+import { resolve } from 'path';
+import { promisify } from 'util';
+import { exec } from 'child_process';
+export const execa = promisify(exec);
+export default async function runExecutor(
+  options: RunExecutorSchema,
+  context: ExecutorContext
+) {
+  logger.info('Run stepci run executor');
+  const workflow = options.workflow || 'workflow';
+  const path = resolve(context.cwd, `${workflow}.yml`);
+  try {
+    logger.info(`Running workflow ${workflow}`);
+    await execa(`stepci run ${path}`);
+  } catch (e) {
+    if (e.stderr.includes('not found')) {
+      logger.error('stepci not found, install it and ensure it is on the path');
+      return;
+    }
+  }
+  return {
+    success: true,
+  };
+}
